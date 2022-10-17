9f00b03dbf2a19220f89bc02ae86bcc7dfe04184
riku0402 <04022005rikuriku@gmail.com>
2022-10-17 09:50:59 +0900

    ファイルを追加

---
+++ Assets/Scripts/CoinPush.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPush : MonoBehaviour
+{
+    [SerializeField]
+    [Header("ÉRÉCÉì")]
+    GameObject _coin;
+
+
+    void Update()
+    {
+        
+    }
+}
